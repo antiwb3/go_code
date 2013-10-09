665eed79a82bdbe8199395ad027af2a06dd6863f
antiwb3 <99143631@qq.com>
2013-10-09 23:25:40 +0800

    commit test
    commit test

---
+++ src/wangbin/hello/test.go
@@ -0,0 +1,16 @@
+package main
+
+import ( 
+	"fmt"
+	)
+
+func main() {
+
+	// var a int
+	var s string
+	s = string(0x1234)
+	fmt.Print("hello world!1")
+	fmt.Print(s)
+	//fmt.Printf("Hello, world.  Sqrt(2) = %v\n", mymath.Sqrt(2))
+	//os.sys("puase")
+}
